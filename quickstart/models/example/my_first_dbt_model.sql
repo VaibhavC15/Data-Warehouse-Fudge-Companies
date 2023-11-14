8ad21d8ebb6ae276a9c651d0942218b4473d0f7e
vachaudh@g.syr.edu <vachaudh@g.syr.edu>
2023-11-14 20:26:20 +0000

    2nd change- main

--- quickstart/models/example/my_first_dbt_model.sql
+++ quickstart/models/example/my_first_dbt_model.sql
@@ -19,9 +19,9 @@ with source_data as (
 
 select *
 from source_data
-
+where id is not null
 /*
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+
