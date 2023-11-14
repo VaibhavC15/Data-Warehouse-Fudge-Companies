525cb1f3589772019b5ef59250c33a321e5bb0d0
vachaudh@g.syr.edu <vachaudh@g.syr.edu>
2023-11-14 19:38:55 +0000

    Add customers model, tests, docs.

--- quickstart/models/customers.sql
+++ quickstart/models/customers.sql
@@ -1,26 +1,12 @@
-use warehouse transforming;
-go 
-
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
